32870f91531651d54dc233e810f5868382f2e164
Notnik_kg <nurbekovaasema04@gmail.com>
2026-06-30 19:28:43 +0600

    Настройка деплоя на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sapremo-factory/', // Укажи здесь имя своего репозитория в слешах
 })
